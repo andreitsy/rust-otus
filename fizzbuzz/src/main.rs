5cfda29a076263f11d3a06400f42dac2bea4c39e
Andrei Tsypilnikov <andrei.tsypilnikov@onetick.com>
2021-11-04 17:01:50 +0300

    soltuion

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -2,18 +2,20 @@ use std::vec::Vec;
 
 fn main() {
     const COUNT: usize = 100;
-    let mut Result = Vec::with_capacity(COUNT);
+    let mut result = Vec::with_capacity(COUNT);
     for i in 1..=COUNT {
         let s = match (i % 3, i % 5) {
-            (0, 0) => String:from("FizzBuzz"), (0, _) => String::from("Fizz"),
-            (_, 0) => String::from("Buzz"), (_, _) => format!("{}", i)
+            (0, 0) => String::from("FizzBuzz"),
+            (0, _) => String::from("Fizz"),
+            (_, 0) => String::from("Buzz"),
+            (_, _) => format!("{}", i),
         };
-        Result.push(s);
+        result.push(s);
     }
-    print_result(&Result);
+    print_result(&result);
 }
 
-fn print_result(result: &Vec<String>) {
+fn print_result(result: &[String]) {
     for r in result {
         println!("{}", r)
     }
